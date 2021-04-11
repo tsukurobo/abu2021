78a68f6cf2e07d772ea14f085fe6323606d6dc13
kinoko <exeforz152@gmail.com>
2021-04-11 19:21:52 +0900

    fixed a bug
    modified not to oscillate easily

--- src/kinematics_model/mecanum_pid_i2c/pid_calculator.h
+++ src/kinematics_model/mecanum_pid_i2c/pid_calculator.h
@@ -1,20 +1,24 @@
 #define NORMAL 1
 #define BUMPLESS 2
+#define TWO_DOF 3
 #define NORMAL_WITH_FF 3
 #define BUMPLESS_WITH_FF 4
 
 typedef struct PIDSettings{
-  float kp, ki, kd, ts, tdel, vmax, vmin, T, K;
+  float kp, ki, kd, ts, tdel, vmax, vmin, a1, a2; //a1, a2はFF制御用
   uint8_t mode;
 };
 
 class PIDCalculator{
 public:
+  float kp, ki, kd, ts, tdel, a1, a2; //a1, a2はフィードフォワード制御用の定数
   PIDCalculator(PIDSettings);
-  float calcValue(float); //誤差から出力を計算
+  float calcValue(float, float); //誤差から出力を計算
+  float getP();
+  float getI();
+  float getD();
   
 private:
-  float kp, ki, kd, ts, tdel, T, K; //T, Kはフィードフォワード制御用の定数
   float vmax, vmin; //アンチワインドアップ発動の閾値(PWMのduty比の最大・最小値)
   float u, pre_e, err_sum; //uは出力
   float P, I, D, D_pre; //各項の値
