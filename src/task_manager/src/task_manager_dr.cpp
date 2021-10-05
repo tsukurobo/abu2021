8b7d5e1389142139919cea4a3f0410389a012dee
tsukurobo-kyoyo <tsukurobo@gmail.com>
2021-10-05 18:18:48 +0900

    DR修正

--- src/task_manager/src/task_manager_dr.cpp
+++ src/task_manager/src/task_manager_dr.cpp
@@ -3,7 +3,7 @@
 #include <std_msgs/Int16.h>
 #include <sensor_msgs/Joy.h>
 #include <abu2021_msgs/cmd_vw.h>
-#include <abu2021_msgs/auto_drive_order.h>
+//#include <abu2021_msgs/auto_drive_order.h>
 #include <abu2021_msgs/turn_and_dist.h>
 
 #define but1 msg->buttons[0]
@@ -15,10 +15,15 @@
 #define butZL msg->buttons[6]
 #define butZR msg->buttons[7]
 
-double TURN_R = 0.9; //回転台操作の旋回半径[m]
+//double TURN_R = 0.9; //回転台操作の旋回半径[m]
+
+double speed_xy = 1.5;
+double speed_w = 1.5;
+double speed_turn = 2.0;
+double turn_rad = 0.9;
 
 ros::Publisher pub;
-ros::Publisher pub_ad;
+//ros::Publisher pub_ad;
 ros::Publisher pub_turn_and_dist;
 
 void get_joy(const sensor_msgs::Joy::ConstPtr& msg);
@@ -30,8 +35,14 @@ int main(int argc, char **argv){
 
 	ros::Subscriber sub = nh.subscribe("joy", 1, get_joy);
 	pub = nh.advertise<abu2021_msgs::cmd_vw>("cmd", 1);
-	pub_ad = nh.advertise<abu2021_msgs::auto_drive_order>("ad_order", 1);
+	//pub_ad = nh.advertise<abu2021_msgs::auto_drive_order>("ad_order", 1);
 	pub_turn_and_dist = nh.advertise<abu2021_msgs::turn_and_dist>("turn_and_dist_order", 1);
+
+	//parameter
+	nh.getParam("speed/xy", speed_xy);
+	nh.getParam("speed/w", speed_w);
+	nh.getParam("speed/turn", speed_turn);
+	nh.getParam("speed/turn_rad", turn_rad);
 
 	ros::spin();
 
@@ -41,21 +52,22 @@ int main(int argc, char **argv){
 // ジョイコン
 void get_joy(const sensor_msgs::Joy::ConstPtr& msg){
 	abu2021_msgs::cmd_vw cmd;
-	abu2021_msgs::auto_drive_order ad_order;
+	//abu2021_msgs::auto_drive_order ad_order;
 	abu2021_msgs::turn_and_dist t_d;
 
 
 	if(butR == 0){ //手動走行
-		cmd.vx = 1.5*msg->axes[1];
-		cmd.vy = 1.5*msg->axes[0];
-		cmd.w  = 1.5*msg->axes[3];
+		cmd.vx = speed_xy*msg->axes[1];
+		cmd.vy = speed_xy*msg->axes[0];
+		cmd.w  = speed_w *msg->axes[3];
 	}else if(butR == 1){ //回転台操作走行
-		cmd.vx = 2.0*msg->axes[2];
+		cmd.vx = speed_turn*msg->axes[2];
 		cmd.vy = 0;
-		cmd.w  = -2.0*msg->axes[2]/TURN_R;
+		cmd.w  = -speed_turn*msg->axes[2]/turn_rad;
 	}
 
 	//自動走行
+	/*
 	ad_order.go = msg->axes[4];
 	if(butL == 1){
 		if     (but1 == 1) ad_order.set1 = 1;
@@ -67,7 +79,7 @@ void get_joy(const sensor_msgs::Joy::ConstPtr& msg){
 	else if(but2 == 1) ad_order.path2 = 1;
 	else if(but3 == 1) ad_order.path3 = 1;
 	else if(but4 == 1) ad_order.path4 = 1;
-
+	*/
 	//回転台操作
 	if(butZR == 1) t_d.solenoid_r = 1;
 	else t_d.solenoid_r = 0;
@@ -79,13 +91,13 @@ void get_joy(const sensor_msgs::Joy::ConstPtr& msg){
 
 	//緊急停止
 	if(butL == 1){
-		ad_order.emg_stop = 1;
+		//ad_order.emg_stop = 1;
 		cmd.vx = 0;
 		cmd.vy = 0;
 		cmd.w  = 0;
 	}
 
 	pub.publish(cmd);
-	pub_ad.publish(ad_order);
+	//pub_ad.publish(ad_order);
 	pub_turn_and_dist.publish(t_d);
 }
--- src/turntable/arduino/joyturntable/joyturntable.ino
+++ src/turntable/arduino/joyturntable/joyturntable.ino
@@ -12,43 +12,44 @@ const int open_leftSOLENOID = 11;
 const int close_leftSOLENOID = 13;
 
 int delay_sol = 10; //delay time of solenoid on [milli sec]
-
 int pw_dist = 0; //妨害の速度
+int addr = 0x21;
+
 ros::NodeHandle nh;
 
 std_msgs::Int16MultiArray mode;
 
-IseMotorDriver md(0x21); //AVRのアドレスに合わせて変更
+IseMotorDriver *md;
 
-void joy_operation(const std_msgs::Int16MultiArray &mode){
+void joy_operation(const std_msgs::Int16MultiArray &mode) {
   //右アームを開く
-  if(mode.data[0]==2){
+  if (mode.data[0] == 2) {
     open_righthand();
   }//右アームを閉じる
-  else if(mode.data[0]==1){
+  else if (mode.data[0] == 1) {
     close_righthand();
   }//右アームをリリース
-  else{
+  else {
     release_righthand();
   }
-  
+
   //左アームを開く
-  if(mode.data[1]==2){
+  if (mode.data[1] == 2) {
     open_lefthand();
   }//左アームを閉じる
-  else if(mode.data[1]==1){
+  else if (mode.data[1] == 1) {
     close_lefthand();
   }//左アームをリリース
-  else{
+  else {
     release_lefthand();
   }
-  
+
   pw_dist = mode.data[3];
   //妨害する
-  if(mode.data[2]==1){
+  if (mode.data[2] == 1) {
     disturb();
   }//妨害しない
-  else{
+  else {
     not_disturb();
   }
 }
@@ -58,18 +59,25 @@ std_msgs::String string_msg;
 ros::Subscriber<std_msgs::Int16MultiArray> sub("turn_and_dist", &joy_operation);
 ros::Publisher chatter("message", &string_msg);
 
-void setup(){
+void setup() {
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(chatter);
 
-  mode.data = (int*)malloc(sizeof(int)*4);
+  /*
+  while(!nh.getParam("/turn_dist/md_addr", &addr)){
+    nh.logerror("could not get param!");
+  };
+  */
+  
+  mode.data = (int*)malloc(sizeof(int) * 4);
   mode.data_length = 4;
 
   IseMotorDriver::begin();
-  if(!md==false) md << IseMotorDriver::createSettingData(PWM_64KHZ,SM_BRAKE_LOW_SIDE);
-  
+  md = new IseMotorDriver((uint8_t)addr);
+  if (!*md == false) *md << IseMotorDriver::createSettingData(PWM_64KHZ, SM_BRAKE_LOW_SIDE);
+
   //init pin
   pinMode(open_rightSOLENOID, OUTPUT);
   pinMode(close_rightSOLENOID, OUTPUT);
@@ -83,14 +91,15 @@ void setup(){
   digitalWrite(close_leftSOLENOID, LOW);
 }
 
-void loop(){
+void loop() {
   nh.spinOnce();
   chatter.publish(&string_msg);
+  if (!*md== true) nh.logerror("md  not connected!");
   delay(delay_sol);
 }
 
 //左アームを開く
-void open_lefthand(){
+void open_lefthand() {
   digitalWrite(open_leftSOLENOID, LOW);
   digitalWrite(close_leftSOLENOID, LOW);
   digitalWrite(open_leftSOLENOID, HIGH);
@@ -98,21 +107,21 @@ void open_lefthand(){
 }
 
 //左アームを閉じる
-void close_lefthand(){
+void close_lefthand() {
   digitalWrite(close_leftSOLENOID, LOW);
   digitalWrite(open_leftSOLENOID, LOW);
   digitalWrite(close_leftSOLENOID, HIGH);   //閉めた後開放しない
   string_msg.data = "Lefthand closed";
 }
 //左アームをリリース
-void release_lefthand(){
+void release_lefthand() {
   digitalWrite(close_leftSOLENOID, LOW);
   digitalWrite(open_leftSOLENOID, LOW);
   string_msg.data = "Lefthand released";
 }
 
 //右アームを開く
-void open_righthand(){
+void open_righthand() {
   digitalWrite(open_rightSOLENOID, LOW);
   digitalWrite(close_rightSOLENOID, LOW);
   digitalWrite(open_rightSOLENOID, HIGH);
@@ -120,26 +129,26 @@ void open_righthand(){
 }
 
 //右アームを閉じる
-void close_righthand(){
+void close_righthand() {
   digitalWrite(close_rightSOLENOID, LOW);
   digitalWrite(open_rightSOLENOID, LOW);
   digitalWrite(close_rightSOLENOID, HIGH);   //閉めた後開放しない
   string_msg.data = "Righthand closed";
 }
 
-//左アームをリリース
-void release_righthand(){
+//右アームをリリース
+void release_righthand() {
   digitalWrite(close_rightSOLENOID, LOW);
   digitalWrite(open_rightSOLENOID, LOW);
   string_msg.data = "Righthand released";
 }
 
 //妨害する
-void disturb(){
-  md << pw_dist;
+void disturb() {
+  *md << pw_dist;
 }
 
 //妨害しない
-void not_disturb(){
-  md << 0;
+void not_disturb() {
+  *md << 0;
 }
